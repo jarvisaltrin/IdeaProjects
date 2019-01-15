93face3c9991fc2d24dd34fd8c55738104e7fc6d
altri <jarvisaltrin@gmail.com>
2019-01-15 02:26:43 -0500

    Updated code of longest common subsequence to print the LCS

--- DynamicProgramming/src/com/company/Main.java
+++ DynamicProgramming/src/com/company/Main.java
@@ -73,7 +73,7 @@ public class Main {
     * For example, the length of LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80} is 6 and
     * LIS is {10, 22, 33, 50, 60, 80}.
     *
-    * Time Complexity O(n^2)
+    * Time Complexity O(n log n)
     * */
     public static int longestIncreasingSubsequenceLength(int[] arr){
         if(arr == null || arr.length == 0){
