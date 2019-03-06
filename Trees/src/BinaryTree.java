ce6d1d3640fffbea42bd21a26ad775f7be760f30
altri <jarvisaltrin@gmail.com>
2019-03-06 11:41:52 -0800

    Added function to find the lowest commonn ancestor of two nodes

--- Trees/src/BinaryTree.java
+++ Trees/src/BinaryTree.java
@@ -285,6 +285,17 @@ public class BinaryTree {
         return 1+Math.max(height(node.left), height(node.right));
     }
 
+    // Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+    // The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both
+    // p and q as descendants (where we allow a node to be a descendant of itself).
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return (left == null) ? right : right == null ? left : root;
+    }
+
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         tree.root = new TreeNode(1);
